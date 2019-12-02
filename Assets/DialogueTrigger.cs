6c0404fd1e0dda18872de3093ba33682185be6cb
lurdan <hastgt@icloud.com>
2019-12-02 13:18:08 +0100

    monologue
    monologue

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject uiObject;
+
+    private void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.F))
+        {
+            uiObject.SetActive(true);
+        }
+
+        else
+        {
+            if (uiObject.SetActive = true && Input.GetKeyDown(KeyCode.Space))
+            {
+                Destroy(uiObject);
+            }
+        }
+    }
+
+
+}
