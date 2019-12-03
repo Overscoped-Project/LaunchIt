7be3b845e922f0c7d99a32f07f97dc4a70d35259
lurdan <hastgt@icloud.com>
2019-12-03 17:43:35 +0100

    some fixes
    some fixes

--- Assets/Dialogues/DialogueTrigger.cs
+++ Assets/Dialogues/DialogueTrigger.cs
@@ -6,12 +6,21 @@ public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
 
-    public void OnTriggerEnter2D(Collider2D other)
+    public void OnTriggerEnter2D (Collider2D other)
     {
         if (other.tag == "Player")
         {
             Debug.Log("Bugged");
             FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
         } 
+    }
+
+    public void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("left");
+            FindObjectOfType<DialogueManager>().EndDialogue();
+        }
     }
 }
