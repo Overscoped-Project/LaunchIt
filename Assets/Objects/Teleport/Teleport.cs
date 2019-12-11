a4a48af4bc1bc7519a27c12b70ca6f7ad945aabc
Gasaini Gasainiev <Gasaini.Gasainiev@games-academy.de>
2019-12-11 20:52:49 +0100

    fixed bugs

--- Assets/Objects/Teleport/Teleport.cs
+++ Assets/Objects/Teleport/Teleport.cs
@@ -7,30 +7,48 @@ public class Teleport : MonoBehaviour
     public GameObject Portal;
     public GameObject Player;
     private IEnumerator coroutine;
+    private bool inTeleport = false;
 
     private void Start()
     {
         coroutine = Teleportation();
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q) && other.gameObject.tag == "Player")
+        if(Input.GetKeyDown(KeyCode.Q) && inTeleport == true)
         {
-            StartCoroutine (coroutine);
+            StartCoroutine(coroutine);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("true");
+            inTeleport = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        inTeleport = false;
         StopCoroutine(coroutine);
         Debug.Log("Stopped");
+        Debug.Log("false");
     }
 
     IEnumerator Teleportation()
     {
-        Debug.Log("Started");
-        yield return new WaitForSeconds(3);
-        Player.transform.position = new Vector3(Portal.transform.position.x, Portal.transform.position.y, 0);
+        while (true)
+        {
+            Debug.Log("Started");
+            yield return new WaitForSeconds(3);
+            Player.transform.position = new Vector3(Portal.transform.position.x, Portal.transform.position.y, 0);
+            yield return null;
+        }
+        
     }
 }
