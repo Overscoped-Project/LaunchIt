81a16625c8d3a8549007fd7ff95fec54929e4320
Viktor Jannicke <viktor.jannicke@games-academy.de>
2019-12-12 18:52:44 +0100

    Small Changes much ToDo
    Small Changes much ToDo for Gate 1

---
+++ Assets/LevelManagerFolder/ExecuteLevelManagerDelayed.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExecuteLevelManagerDelayed : MonoBehaviour
+{
+    public float delaySeconds;
+    float timer;
+    public enum Level {
+        MainMenu,
+        Settings,
+        Intro,
+        Game,
+        Outro,
+        Credits
+    }
+    public Level level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > delaySeconds)
+        {
+            SceneManager.LoadSceneAsync((int)level, LoadSceneMode.Single);
+        }
+    }
+}
