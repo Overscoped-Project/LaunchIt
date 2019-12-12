c3fe72da85a2555fc770f4c957e1c3d0389bd097
lurdan <hastgt@icloud.com>
2019-12-12 15:16:57 +0100

    polished
    polished

--- Assets/Objects/Teleport/Teleport.cs
+++ Assets/Objects/Teleport/Teleport.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    public GameObject portal;
+    
+    public SpriteRenderer portal;
     public GameObject player;
     private IEnumerator coroutine;
     private bool inTeleport = false;
+    public float timeToTeleport;
 
 
-  
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Q) && inTeleport == true)
@@ -32,16 +33,20 @@ public class Teleport : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        inTeleport = false;
-        StopCoroutine(coroutine);
-        Debug.Log("false trigger exit");
+        if (other.gameObject.tag == "Player")
+        {
+            inTeleport = false;
+            StopCoroutine(coroutine);
+            Debug.Log("false trigger exit");
+        }
     }
 
     IEnumerator Teleportation()
     {
             Debug.Log("started teleport");
-            yield return new WaitForSeconds(3);
-            player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y - 15);
+            yield return new WaitForSeconds(timeToTeleport);
+            player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y - portal.bounds.extents.magnitude);
+          
 
     }
 }
