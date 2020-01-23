3ed724cee54d2c485080051c17e9df11a83634bc
Viktor Jannicke <viktor.jannicke@games-academy.de>
2020-01-23 12:57:20 +0100

    Dialog

---
+++ Assets/Cutscenes/TextWriter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TextWriter : MonoBehaviour
+{
+
+    public string[] text;
+    public float timeTillNextTextPart;
+    public TextMeshProUGUI tGUI;
+    public enum Level
+    {
+        MainMenu,
+        Settings,
+        Intro,
+        Game,
+        Outro,
+        Credits,
+        None
+    }
+    public Level level;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float timer = 0;
+    public int count = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= timeTillNextTextPart && count < text.Length)
+        {
+            timer = 0;
+            tGUI.text = text[count];
+            count++;
+        }
+        else if (timer != timeTillNextTextPart && count < text.Length)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            if(level != Level.None)
+            {
+                SceneManager.LoadScene((int)level, LoadSceneMode.Single);
+            }
+        }
+    }
+}
